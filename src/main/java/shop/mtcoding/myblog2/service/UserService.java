ae869a11e6110b73709cebdc78516d9c37446e23
tero1115 <limjisang1115@gmail.com>
2023-02-12 21:00:11 +0900

    회원가입 테스트 완료

--- src/main/java/shop/mtcoding/myblog2/service/UserService.java
+++ src/main/java/shop/mtcoding/myblog2/service/UserService.java
@@ -1,7 +1,10 @@
 package shop.mtcoding.myblog2.service;
 
+import org.springframework.stereotype.Service;
+
 import shop.mtcoding.myblog2.dto.user.UserReq.JoinReqDto;
 
+@Service
 public class UserService {
 
     public void 회원가입(JoinReqDto joinReqDto) {
