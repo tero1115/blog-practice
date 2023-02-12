5803a6fb5b82a8cf1bd68e6073b2fae14c4fbc29
tero1115 <limjisang1115@gmail.com>
2023-02-12 20:45:22 +0900

    더미데이터 생성, db 테이블 생성, 모델링 완료

---
+++ src/main/java/shop/mtcoding/myblog2/model/UserRepository.java
@@ -0,0 +1,18 @@
+package shop.mtcoding.myblog2.model;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public User findById(int id);
+
+    public int updateById(@Param("id") int id, @Param("password") String password, @Param("email") String eamil);
+
+    public int deleteById(int id);
+
+}
