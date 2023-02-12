903c12bafac14ecbcd9e430d615f032304e115be
tero1115 <limjisang1115@gmail.com>
2023-02-12 21:54:55 +0900

    게시글목록보기 컨트롤러 추가

--- src/test/java/shop/mtcoding/myblog2/controller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/myblog2/controller/UserControllerTest.java
@@ -6,9 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import javax.servlet.http.HttpSession;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
