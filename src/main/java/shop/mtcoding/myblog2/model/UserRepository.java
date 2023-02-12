706debf92f8ccd3d8457e77ff50fa3dee64e55cb
tero1115 <67888405+tero1115@users.noreply.github.com>
2023-02-12 21:06:03 +0900

    회원가입 완료 (#1)
    * 유저 컨트롤러 추가
    
    * 회원가입 테스트 완료
    
    * 회원가입 서비스, 매퍼, 리포지토리 추가

--- src/main/java/shop/mtcoding/myblog2/model/UserRepository.java
+++ src/main/java/shop/mtcoding/myblog2/model/UserRepository.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserRepository {
 
+    public User findByUsername(@Param("username") String username);
+
     public int insert(@Param("username") String username, @Param("password") String password,
             @Param("email") String email);
 
---
+++ src/test/java/shop/mtcoding/myblog2/controller/UserControllerTest.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.myblog2.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void join_test() throws Exception {
+        // given
+        String requestBody = "username=cos&password=1234&email=cos@nate.com";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/join").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+}
