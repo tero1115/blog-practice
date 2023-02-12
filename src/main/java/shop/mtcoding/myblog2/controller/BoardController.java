18737ae7a86f9e0a2022a31cc6bb0fd417a91b17
tero1115 <67888405+tero1115@users.noreply.github.com>
2023-02-12 22:16:12 +0900

    게시글 삭제 완료 (#6)
    * 게시글 삭제 컨트롤러 추가
    
    * 게시글 삭제 테스트 완료
    
    * 게시글 삭제 서비스 추가

--- src/main/java/shop/mtcoding/myblog2/controller/BoardController.java
+++ src/main/java/shop/mtcoding/myblog2/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,16 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @DeleteMapping("/board/{id}")
+    public @ResponseBody ResponseEntity<?> delete(@PathVariable int id) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        boardService.게시글삭제(id, principal.getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "삭제성공", null), HttpStatus.OK);
+    }
 
     @PostMapping("/board")
     public @ResponseBody ResponseEntity<?> save(@RequestBody BoardSaveReqDto boardSaveReqDto) {
