eed2f98d5f04365d411461fdf4fd1a25108d8e91
tero1115 <limjisang1115@gmail.com>
2023-02-12 20:54:12 +0900

    유저 컨트롤러 추가

--- src/main/java/shop/mtcoding/myblog2/controller/UserController.java
+++ src/main/java/shop/mtcoding/myblog2/controller/UserController.java
@@ -1,13 +1,40 @@
 package shop.mtcoding.myblog2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.myblog2.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.myblog2.handler.ex.CustomException;
+import shop.mtcoding.myblog2.service.UserService;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/join")
+    public String join(JoinReqDto joinReqDto) {
+
+        if (joinReqDto.getUsername() == null || joinReqDto.getUsername().isEmpty()) {
+            throw new CustomException("username을 작성해주세요");
+        }
+        if (joinReqDto.getPassword() == null || joinReqDto.getPassword().isEmpty()) {
+            throw new CustomException("password를 작성해주세요");
+        }
+        if (joinReqDto.getEmail() == null || joinReqDto.getEmail().isEmpty()) {
+            throw new CustomException("email을 작성해주세요");
+        }
+
+        userService.회원가입(joinReqDto);
+
+        return "redirect:/loginForm";
+    }
+
     @GetMapping("/joinForm")
-    public String joinForm() {
+    public String joinForm(JoinReqDto joinReqDto) {
         return "user/joinForm";
     }
 
---
+++ src/main/java/shop/mtcoding/myblog2/service/UserService.java
@@ -0,0 +1,10 @@
+package shop.mtcoding.myblog2.service;
+
+import shop.mtcoding.myblog2.dto.user.UserReq.JoinReqDto;
+
+public class UserService {
+
+    public void 회원가입(JoinReqDto joinReqDto) {
+    }
+
+}
